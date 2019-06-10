bc9a7d555bd5d90a37964d7296cda0297be6fb71
zhouzhongqing <1030907690@qq.com>
2019-06-10 19:11:10 +0800

    activity

--- app/src/main/java/com/zzq/adb/adbshell/MainActivity.java
+++ app/src/main/java/com/zzq/adb/adbshell/MainActivity.java
@@ -2,7 +2,6 @@ package com.zzq.adb.adbshell;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Instrumentation;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -19,22 +18,16 @@ public class MainActivity extends AppCompatActivity {
 
     private Button newsInfoBut;
 
+    private Button videoBut;
+
+    private Button bookBut;
+
+    private Button stopBtn;
+
     private final String command = "command";
 
-    // 向下滑动随机
-    private final String adb_command_swipe_random = " input swipe 100 700 100 %s";
 
-    // 向下滑动
-    private final String adb_command_swipe = "input swipe 100 700 100 %s";
-
-
-    //返回
-    private final String adb_command_back = "input keyevent 4";
-
-
-    //点击
-    private final String adb_command_click = "input tap 200 300";
-
+    private volatile boolean isRuning = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +46,9 @@ public class MainActivity extends AppCompatActivity {
      **/
     private void initializeView() {
         this.newsInfoBut = findViewById(R.id.button1);
-
+        this.videoBut = findViewById(R.id.button2);
+        this.bookBut = findViewById(R.id.button3);
+        this.stopBtn = findViewById(R.id.stopAll);
     }
 
     /**
@@ -78,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private Handler handlerNewsInfo = new Handler() {
+    private Handler genericHandler = new Handler() {
         public void handleMessage(android.os.Message msg) {
             switch (msg.what) {
                 case 1:
@@ -113,6 +108,21 @@ public class MainActivity extends AppCompatActivity {
     };
 
 
+    private void waitComplete() throws Exception {
+
+        for (int i = 20; i > 0; i--) {
+            Thread.sleep(1000);
+            Message msg = new Message();
+            Bundle bundle = new Bundle();
+            bundle.putInt("time", i);
+            msg.setData(bundle);
+            msg.what = 0;
+            genericHandler.sendMessage(msg);
+            Log.d("ces", "倒计时" + i);
+        }
+
+    }
+
     private void listener() {
         newsInfoBut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -122,25 +132,31 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         try {
-                            for (int i = 20; i > 0; i--) {
-                                Thread.sleep(1000);
+
+                            // 向下滑动随机
+                            String adb_command_swipe_random = " input swipe 100 700 100 %s";
+
+                            // 向下滑动
+                            String adb_command_swipe = "input swipe 100 700 100 %s";
+
+
+                            //返回
+                            String adb_command_back = "input keyevent 4";
+
+
+                            //点击
+                            String adb_command_click = "input tap 200 300";
+
+                            waitComplete();
+
+                            while (isRuning) {
+
                                 Message msg = new Message();
                                 Bundle bundle = new Bundle();
-                                bundle.putInt("time", i);
-                                msg.setData(bundle);
-                                msg.what = 0;
-                                handlerNewsInfo.sendMessage(msg);
-                                Log.d("ces", "倒计时" + i);
-                            }
-
-                            while (true) {
-
-                                Message msg = new Message();
-                                Bundle bundle = new Bundle();
-                                bundle.putString(command, adb_command_back);
+                                bundle.putString(command, adb_command_click);
                                 msg.setData(bundle);
                                 msg.what = 1;
-                                handlerNewsInfo.sendMessage(msg);
+                                genericHandler.sendMessage(msg);
 
                                 int randomSwipe = randInt(10, 20);
                                 for (int i = 0; i < randomSwipe; i++) {
@@ -152,7 +168,7 @@ public class MainActivity extends AppCompatActivity {
                                     bundle.putString(command, String.format(adb_command_swipe_random, swipe_random));
                                     msg.setData(bundle);
                                     msg.what = 1;
-                                    handlerNewsInfo.sendMessage(msg);
+                                    genericHandler.sendMessage(msg);
                                 }
 
 
@@ -161,7 +177,7 @@ public class MainActivity extends AppCompatActivity {
                                 bundle.putString(command, adb_command_back);
                                 msg.setData(bundle);
                                 msg.what = 1;
-                                handlerNewsInfo.sendMessage(msg);
+                                genericHandler.sendMessage(msg);
                                 // 确保退出再点一次退出
                                 Thread.sleep(2000);
                                 msg = new Message();
@@ -169,7 +185,7 @@ public class MainActivity extends AppCompatActivity {
                                 bundle.putString(command, adb_command_back);
                                 msg.setData(bundle);
                                 msg.what = 1;
-                                handlerNewsInfo.sendMessage(msg);
+                                genericHandler.sendMessage(msg);
                                 //等2秒再强制退出
                                 Thread.sleep(2000);
                                 msg = new Message();
@@ -177,7 +193,7 @@ public class MainActivity extends AppCompatActivity {
                                 bundle.putString(command, adb_command_back);
                                 msg.setData(bundle);
                                 msg.what = 1;
-                                handlerNewsInfo.sendMessage(msg);
+                                genericHandler.sendMessage(msg);
                                 //再往下滑动
                                 int swipe_random_back = randInt(400, 450);
                                 msg = new Message();
@@ -185,7 +201,7 @@ public class MainActivity extends AppCompatActivity {
                                 bundle.putString(command, String.format(adb_command_swipe, swipe_random_back));
                                 msg.setData(bundle);
                                 msg.what = 1;
-                                handlerNewsInfo.sendMessage(msg);
+                                genericHandler.sendMessage(msg);
 
                             }
 
@@ -198,6 +214,80 @@ public class MainActivity extends AppCompatActivity {
                 }.start();
 
 
+            }
+        });
+
+
+        videoBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Thread() {
+                    @Override
+                    public void run() {
+                        try {
+                            waitComplete();
+
+                            while (isRuning) {
+                                int loopCount = randInt(10, 20);
+                                for (int i = 0; i < loopCount; i++) {
+                                    Thread.sleep(1000);
+                                }
+
+                                // 向下滑动随机
+                                String adb_command_swipe_random = " input swipe 100 700 100 %s";
+
+                                // 返回
+                                String adb_command_back = "input keyevent 4";
+
+
+                                int swipe_random = randInt(350, 450);
+                                Message msg = new Message();
+                                Bundle bundle = new Bundle();
+                                bundle.putString(command, String.format(adb_command_swipe_random, swipe_random));
+                                msg.setData(bundle);
+                                msg.what = 1;
+                                genericHandler.sendMessage(msg);
+
+
+                                // 尝试退出
+                                // 返回
+                                msg = new Message();
+                                bundle = new Bundle();
+                                bundle.putString(command, adb_command_back);
+                                msg.setData(bundle);
+                                msg.what = 1;
+                                genericHandler.sendMessage(msg);
+                                //确保退出再点一次退出
+                                Thread.sleep(2000);
+                                msg = new Message();
+                                bundle = new Bundle();
+                                bundle.putString(command, adb_command_back);
+                                msg.setData(bundle);
+                                msg.what = 1;
+                                genericHandler.sendMessage(msg);
+                                // 等2秒再强制退出
+                                Thread.sleep(2000);
+                                msg = new Message();
+                                bundle = new Bundle();
+                                bundle.putString(command, adb_command_back);
+                                msg.setData(bundle);
+                                msg.what = 1;
+                                genericHandler.sendMessage(msg);
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }.start();
+            }
+        });
+
+
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isRuning = false;
             }
         });
     }
